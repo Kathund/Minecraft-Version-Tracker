be2e5a0aa80157204dc4597f59517294b21ab743
Jacob <kathund@proton.me>
2026-02-16 19:58:47 +0800

    refactor(Embed): Colors

--- src/Discord/Private/Templates/Embed.ts
+++ src/Discord/Private/Templates/Embed.ts
@@ -1,9 +1,12 @@
 import { EmbedBuilder } from 'discord.js';
 
+// eslint-disable-next-line import/exports-last
+export const Colors = { Blue: 0x17bebb, Green: 0x00ff3c, Yellow: 0xffbc1f, Red: 0xed474a, Pink: 0xff70a2 };
+
 class Embed extends EmbedBuilder {
   constructor() {
     super();
-    this.setColor('#ff538e');
+    this.setColor(Colors.Pink);
     this.setTimestamp();
     this.setFooter({ text: 'Minecraft Version Tracker by @.kathund', iconURL: 'https://i.imgur.com/uUuZx2E.png' });
   }
@@ -12,8 +15,8 @@ class Embed extends EmbedBuilder {
 export class ErrorEmbed extends Embed {
   constructor() {
     super();
+    this.setColor(Colors.Red);
     this.setAuthor({ name: 'Something went wrong' });
-    this.setColor(15548997);
   }
 }
 
