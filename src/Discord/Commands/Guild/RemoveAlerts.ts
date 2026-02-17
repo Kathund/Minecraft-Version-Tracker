fdad532c931bd722a7d449387ea5a5605d5fac1d
Jacob <kathund@proton.me>
2026-02-17 19:55:19 +0800

    feat(/remove-alerts)
    Allows you to remove an alert from a channel or something idk

--- src/Discord/Commands/Guild/RemoveAlerts.ts
+++ src/Discord/Commands/Guild/RemoveAlerts.ts
@@ -2,17 +2,17 @@ import Command from '../../Private/Commands/Command.js';
 import CommandData from '../../Private/Commands/CommandData.js';
 import Embed from '../../Private/Templates/Embed.js';
 import MinecraftVersionTrackerError from '../../../Private/Error.js';
-import { ChannelType, type ChatInputCommandInteraction, PermissionFlagsBits } from 'discord.js';
+import { type ChatInputCommandInteraction, PermissionFlagsBits } from 'discord.js';
 import { type VersionType, VersionTypeKeys, VersionTypeLabels } from '../../../Mongo/Version/Schema.js';
 import type DiscordManager from '../../DiscordManager.js';
 import type { Server } from '../../../Mongo/Server/Schema.js';
 
-class SetupAlerts extends Command {
+class RemoveAlerts extends Command {
   constructor(discord: DiscordManager) {
     super(discord);
     this.data = new CommandData()
-      .setName('setup-alerts')
-      .setDescription('Setup alerts')
+      .setName('remove-alerts')
+      .setDescription('Remove alerts')
       .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
       .addStringOption((option) =>
         option
@@ -20,49 +20,21 @@ class SetupAlerts extends Command {
           .setDescription('Type of notification')
           .setRequired(true)
           .addChoices(...Object.entries(VersionTypeLabels).map(([value, name]) => ({ name, value })))
-      )
-      .addChannelOption((option) =>
-        option
-          .setName('channel')
-          .setDescription('the channel to post in (default current)')
-          .addChannelTypes(
-            ChannelType.GuildText,
-            ChannelType.GuildAnnouncement,
-            ChannelType.PublicThread,
-            ChannelType.PrivateThread
-          )
-      )
-      .addRoleOption((option) => option.setName('role').setDescription('A role to be mentioned on a new alert'));
+      );
   }
 
   override async execute(interaction: ChatInputCommandInteraction): Promise<void> {
     if (interaction.guild === null || interaction.guild === undefined) {
       throw new MinecraftVersionTrackerError('Please run this in a guild');
     }
-    if (interaction.channel === null || interaction.channel === undefined) {
-      throw new MinecraftVersionTrackerError('Please run this in a channel');
-    }
 
     const type = (interaction.options.getString('type') ?? 'release') as VersionType;
-    const channel = interaction.options.getChannel('channel') ?? interaction.channel;
-    const channelData = await interaction.guild.channels.fetch(channel.id);
-    if (channelData === null || !channelData.isSendable()) {
-      throw new MinecraftVersionTrackerError(`<#${channel.id}> is not sendable or doesn't exist`);
-    }
-    const msgPermission = await this.discord.utils.checkMessagePermissionsInChannel(channelData);
-    if (!msgPermission) {
-      throw new MinecraftVersionTrackerError(
-        'I cannot send messages in that channel. Please select a different channel'
-      );
-    }
 
-    const role = interaction.options.getRole('role');
     const existing = await this.discord.Application.mongo.server.getItem(interaction.guild.id);
 
     const versionData = {} as Pick<Server, VersionType>;
     for (const key of VersionTypeKeys) {
-      versionData[key] =
-        key === type ? { channel: channel.id, role: role?.id ?? null } : (existing.data?.[key] ?? undefined);
+      versionData[key] = key === type ? undefined : (existing.data?.[key] ?? undefined);
     }
 
     const save = await this.discord.Application.mongo.server.saveItem({ id: interaction.guild.id, ...versionData });
@@ -87,4 +59,4 @@ class SetupAlerts extends Command {
   }
 }
 
-export default SetupAlerts;
+export default RemoveAlerts;
