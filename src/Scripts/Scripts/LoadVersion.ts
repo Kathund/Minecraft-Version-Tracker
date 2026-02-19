9f55c449e8320ba115c180fee1dc08cc2427e3be
Jacob <kathund@proton.me>
2026-02-19 15:07:46 +0800

    refactor(LoadVersions)
    Now uses the fetch verisons function

--- src/Scripts/Scripts/LoadVersion.ts
+++ src/Scripts/Scripts/LoadVersion.ts
@@ -1,13 +1,9 @@
 import Script from '../Private/Script.js';
-import type { Version } from '../../Mongo/Version/Schema.js';
 
 class LoadVersions extends Script {
   override async execute(): Promise<void> {
     console.other('Loading all minecraft versions in to the database');
-    const res = await this.scriptManager.Application.requestHandler.request(
-      'https://piston-meta.mojang.com/mc/game/version_manifest_v2.json'
-    );
-    const versions: Version[] = res.data.versions;
+    const versions = await this.scriptManager.Application.minecraftUtils.getVersions();
     for (const version of versions.reverse()) {
       this.scriptManager.Application.mongo.version
         .saveItem(await this.scriptManager.Application.minecraftUtils.convertVersion(version))
